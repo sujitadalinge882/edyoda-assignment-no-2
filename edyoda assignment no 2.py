832f54d062876a782b8a4afcce8b3c8ca5a5cabb
sujit vijay adalinge <110627737+sujitadalinge882@users.noreply.github.com>
2022-08-14 16:45:45 +0530

    Add files via upload

---
+++ edyoda assignment no 2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+word= input("input a word to revrs")
+for char in range(len(word) -1,-1,-1):
+    print (word[char],end=" ")
+     print("\n")
+
+
+# In[ ]:
+
+
+s = "sujit"
+print(::-1)
+
+
+# In[ ]:
+
+
+x=1
+id(x)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
